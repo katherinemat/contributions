e959c94b729280a1de0811ed743d5687c2174b3d
Katy Daviscourt and Kaz Matthews <katy+kaz@>
2017-04-10 13:46:08 -0700

    get list of contributors printed on page

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -9,14 +9,16 @@ import { CrpService } from '../crp.service';
 })
 export class SearchComponent {
   constructor(private crpService: CrpService) {}
-    donations = {};
+    donations = [];
 
     getDonations() {
       this.crpService.getUser()
       .subscribe(
-        data => { console.log(data.response.industries.industry[0]),
-        console.log(data.response.industries.industry[2]["@attributes"].industry_name),
-        this.donations = JSON.stringify(data.response.industries.industry[2]["@attributes"].industry_name),
+        data => {
+        for (var i = 0; i < data.response.industries.industry.length; i++){
+          this.donations.push(data.response.industries.industry[i]["@attributes"].industry_name)
+        }
+        // this.donations = JSON.stringify(data.response.industries.industry[2]["@attributes"].industry_name),
         error => alert(error) }
       );
     }
