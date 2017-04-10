fb6df57e99bfadb6d7a168b7998d1c8dea0832d6
Katy Daviscourt and Kaz Matthews <katy+kaz@>
2017-04-10 09:19:57 -0700

    add results and twitter components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,17 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
 import { routing } from './app.routing';
+import { MainComponent } from './main/main.component';
+import { ResultsComponent } from './results/results.component';
+import { TwitterComponent } from './twitter/twitter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchComponent
+    SearchComponent,
+    MainComponent,
+    ResultsComponent,
+    TwitterComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,10 +1,15 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SearchComponent } from './search/search.component';
+import { MainComponent } from './main/main.component';
 
 const appRoutes: Routes = [
   {
     path: '',
+    component: MainComponent
+  },
+  {
+    path: 'search',
     component: SearchComponent
   }
 ];
