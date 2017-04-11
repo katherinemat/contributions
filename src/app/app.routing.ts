72f7d32003e83c65a880cac4354188853bbd490e
Katy Daviscourt and Nicole Sanders <katy+nicole@>
2017-04-11 11:15:23 -0700

    fix merge conflict

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -19,11 +19,7 @@ const appRoutes: Routes = [
     component: TwitterComponent
   },
   {
-<<<<<<< HEAD
     path: 'org/:id',
-=======
-    path: 'org-detail',
->>>>>>> ea8815e90b61d536314755d5f00063652075f89c
     component: OrgDetailComponent
   }
 ];
