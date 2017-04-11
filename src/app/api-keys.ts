6143b3298775f0c648690e19f9a85db0fc6df89b
Caitlin Hines and Kaz Matthews <caitlin+kaz@>
2017-04-11 13:16:54 -0700

    remove firebase keys from gitignore

---
+++ src/app/api-keys.ts
@@ -0,0 +1,8 @@
+export var masterFirebaseConfig = {
+  apiKey: "AIzaSyA671mnUz2unCQR5KPJLP2zeUfVMPu24uw",
+  authDomain: "contributions-1eff3.firebaseapp.com",
+  databaseURL: "https://contributions-1eff3.firebaseio.com",
+  projectId: "contributions-1eff3",
+  storageBucket: "contributions-1eff3.appspot.com",
+  messagingSenderId: "468548117842"
+};
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http, Headers} from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Stock Political Contributions';
+  searchquery = '';
+  tweetsdata;
+
+  makecall() {
+    var headers = new Headers();
+
+    headers.append('Content-Type', 'application/X-www-form-urlencoded');
+    this.http.post('http://localhost:3000/authorize', {headers: headers}).subscribe((res) => {
+      console.log(res);
+    })
+  }
+  searchcall(){
+    var headers = new Headers();
+    var searchterm = 'query=' + this.searchquery;
+
+    headers.append('Content-Type', 'application/X-www-form-urlencoded');
+
+    this.http.post('http://localhost:3000/search', searchterm, {headers: headers}).subscribe((res) => {
+      this.tweetsdata = res.json().data.statuses;
+    });
+  }
 }
