c9debec974f74e06b14807fde3440d655e0c9436
Caitlin Hines and Kaz Matthews <caitlin+kaz@>
2017-04-11 13:22:16 -0700

    change app component changed to be functional again

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Http, Headers} from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -8,25 +7,4 @@ import { Http, Headers} from '@angular/http';
 })
 export class AppComponent {
   title = 'Stock Political Contributions';
-  searchquery = '';
-  tweetsdata;
-
-  makecall() {
-    var headers = new Headers();
-
-    headers.append('Content-Type', 'application/X-www-form-urlencoded');
-    this.http.post('http://localhost:3000/authorize', {headers: headers}).subscribe((res) => {
-      console.log(res);
-    })
-  }
-  searchcall(){
-    var headers = new Headers();
-    var searchterm = 'query=' + this.searchquery;
-
-    headers.append('Content-Type', 'application/X-www-form-urlencoded');
-
-    this.http.post('http://localhost:3000/search', searchterm, {headers: headers}).subscribe((res) => {
-      this.tweetsdata = res.json().data.statuses;
-    });
-  }
 }
